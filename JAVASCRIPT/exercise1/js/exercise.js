6b7bf08b499fa824eb74742df22a20774a2c6f79
injecti0n <9992685+injecti0n@users.noreply.github.com>
2020-11-03 08:02:51 +0100

    updated

---
+++ JAVASCRIPT/exercise1/js/exercise.js
@@ -0,0 +1,20 @@
+let title = document.getElementById('title');
+
+
+function mouseOver(){
+
+    title.innerText=title.innerText.toUpperCase()
+    console.log(title.innerText.toUpperCase())
+}
+
+function mouseOut(){
+    title.innerText=title.innerText.toLowerCase();
+    console.log(title.innerText.toLowerCase())
+
+}
+
+title.addEventListener('mouseover',mouseOver);
+title.addEventListener('mouseout',mouseOut)
+
+// mouseOver() // JAVASCRIPT
+// mouseOut() // javascript
