46d4f571146d84d2b9893cf2075554e25c2f5a24
injecti0n <9992685+injecti0n@users.noreply.github.com>
2020-11-12 11:18:18 +0100

    while, do while, for loops

---
+++ JAVASCRIPT/tutorial17/app.js
@@ -0,0 +1,99 @@
+/*
+
+while
+do while
+for
+for/in
+for/of
+
+*/
+
+// while loop
+
+// let students = [
+//     {name:"Michael",age:20},
+//     {name:"George",age:19},
+//     20,
+//     "Test"
+// ]
+
+// // console.log(students.length)
+
+
+// let i=0
+// console.log(students.length-1)
+// while(i<students.length){ // condition 
+//     // code
+//     console.log(students[i].name)
+
+    
+//     i++; 
+    
+// }
+
+// while(i<100){
+//     i++;
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// var answer;
+// var result=0;
+
+// while(answer%2!=0){
+//     answer = prompt('Enter a number!');
+//     result += parseInt(answer);
+//     // result = result + parseInt(answer);
+  
+// }
+
+// console.log(parseInt(result),"result")
+
+// do while loop
+
+// do{
+//     // code block
+//    }while(condition)
+   
+// var i=50
+// do{
+//     console.log(i)
+//     i++;
+// }while(i<=10);
+
+// var randomNumber;
+
+// do{
+//     randomNumber= Math.floor(Math.random()*100)+1
+//     console.log(randomNumber)
+// }while(randomNumber>=50)
+
+
+// var number;
+// var sum=0;
+
+// do{
+//     number = Number(prompt('Enter a number!'))
+//     sum+=number;
+//     console.log("Sum Result=>",sum)
+//     console.log(number)
+//     console.log(number!=0)
+// }while(number!=0)
+
+// var password= "123456"
+// var rateLimit=3;
+
+// while(rateLimit>0){
+//     var askUserPassword = prompt('Enter your password')
+//     rateLimit--;
+//     if(askUserPassword == password){
+//         console.log('welcome user')
+//         break;
+//     }else{
+//         console.log(`You've ${rateLimit} attemps left.`)
+//     }
+//     console.log(rateLimit)
+//     console.log(askUserPassword)
+// }
+
