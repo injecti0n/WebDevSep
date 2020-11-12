e70517df87b0234567078bc22185bd2559612459
injecti0n <9992685+injecti0n@users.noreply.github.com>
2020-11-12 11:38:26 +0100

    nested for loop

--- JAVASCRIPT/tutorial17/app.js
+++ JAVASCRIPT/tutorial17/app.js
@@ -1,99 +1,144 @@
-/*
+// /*
 
-while
-do while
-for
-for/in
-for/of
+// while
+// do while
+// for
+// for/in
+// for/of
 
-*/
+// */
 
-// while loop
+// // while loop
 
-// let students = [
-//     {name:"Michael",age:20},
-//     {name:"George",age:19},
-//     20,
-//     "Test"
-// ]
+// // let students = [
+// //     {name:"Michael",age:20},
+// //     {name:"George",age:19},
+// //     20,
+// //     "Test"
+// // ]
 
-// // console.log(students.length)
+// // // console.log(students.length)
 
 
-// let i=0
-// console.log(students.length-1)
-// while(i<students.length){ // condition 
-//     // code
-//     console.log(students[i].name)
+// // let i=0
+// // console.log(students.length-1)
+// // while(i<students.length){ // condition 
+// //     // code
+// //     console.log(students[i].name)
 
     
-//     i++; 
+// //     i++; 
     
-// }
+// // }
 
-// while(i<100){
-//     i++;
-//     if(i%2==0){
-//         console.log(i)
-//     }
-// }
+// // while(i<100){
+// //     i++;
+// //     if(i%2==0){
+// //         console.log(i)
+// //     }
+// // }
 
-// var answer;
-// var result=0;
+// // var answer;
+// // var result=0;
 
-// while(answer%2!=0){
-//     answer = prompt('Enter a number!');
-//     result += parseInt(answer);
-//     // result = result + parseInt(answer);
+// // while(answer%2!=0){
+// //     answer = prompt('Enter a number!');
+// //     result += parseInt(answer);
+// //     // result = result + parseInt(answer);
   
-// }
+// // }
 
-// console.log(parseInt(result),"result")
+// // console.log(parseInt(result),"result")
 
-// do while loop
+// // do while loop
 
-// do{
-//     // code block
-//    }while(condition)
+// // do{
+// //     // code block
+// //    }while(condition)
    
-// var i=50
-// do{
-//     console.log(i)
-//     i++;
-// }while(i<=10);
+// // var i=50
+// // do{
+// //     console.log(i)
+// //     i++;
+// // }while(i<=10);
 
-// var randomNumber;
+// // var randomNumber;
 
-// do{
-//     randomNumber= Math.floor(Math.random()*100)+1
-//     console.log(randomNumber)
-// }while(randomNumber>=50)
+// // do{
+// //     randomNumber= Math.floor(Math.random()*100)+1
+// //     console.log(randomNumber)
+// // }while(randomNumber>=50)
 
 
-// var number;
-// var sum=0;
+// // var number;
+// // var sum=0;
 
-// do{
-//     number = Number(prompt('Enter a number!'))
-//     sum+=number;
-//     console.log("Sum Result=>",sum)
-//     console.log(number)
-//     console.log(number!=0)
-// }while(number!=0)
+// // do{
+// //     number = Number(prompt('Enter a number!'))
+// //     sum+=number;
+// //     console.log("Sum Result=>",sum)
+// //     console.log(number)
+// //     console.log(number!=0)
+// // }while(number!=0)
 
-// var password= "123456"
-// var rateLimit=3;
+// // var password= "123456"
+// // var rateLimit=3;
 
-// while(rateLimit>0){
-//     var askUserPassword = prompt('Enter your password')
-//     rateLimit--;
-//     if(askUserPassword == password){
-//         console.log('welcome user')
-//         break;
-//     }else{
-//         console.log(`You've ${rateLimit} attemps left.`)
-//     }
-//     console.log(rateLimit)
-//     console.log(askUserPassword)
+// // while(rateLimit>0){
+// //     var askUserPassword = prompt('Enter your password')
+// //     rateLimit--;
+// //     if(askUserPassword == password){
+// //         console.log('welcome user')
+// //         break;
+// //     }else{
+// //         console.log(`You've ${rateLimit} attemps left.`)
+// //     }
+// //     console.log(rateLimit)
+// //     console.log(askUserPassword)
+// // }
+
+// // for loop
+// let i=0
+// for(i;i<=10;i++){
+//    // console.log('Count:',i)
 // }
 
+// for(let i=1;i<=50;i++){
+//     var oddNumber = i%2;
+//     if(oddNumber!=0){
+//        // console.log(i)
+
+//     }
+// }
+
+// for(let i=1;i<=100;i++){
+//     let res = i*i;
+//    // console.log(res)
+// }
+
+// // let x,y
+// // for(x=1,y=50; x<=10 && y>=50;x++){
+// //     console.log(x,":",y)
+// //     y++
+   
+// // }
+
+
+
+var x,y;
+/// nested for loop
+for(x=9;x<=10;x++){
+
+
+    for(y=1;y<=10;y++){
+
+
+        var result = `${x} * ${y} = ${x*y}` 
+        console.log(result)
+
+    }
+
+
+
+
+}
