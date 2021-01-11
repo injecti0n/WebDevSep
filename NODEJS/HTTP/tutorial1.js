9615a3e6403c13db201ab3fcd2a168aa7433530b
injecti0n <9992685+injecti0n@users.noreply.github.com>
2021-01-11 11:30:21 +0100

    http server

--- NODEJS/HTTP/tutorial1.js
+++ NODEJS/HTTP/tutorial1.js
@@ -5,11 +5,12 @@ const users = require('./modules/users')
 
 http.createServer(function(req,res){
     // res.write('Welcome to my page / intecbrussel') // data we printed in the page
-    res.writeHead(200,{'Content-Type':"text/html"})
-    res.write(req.url)
-   // users().then(response=>JSON.stringify(response))
-    //res.write('[1,2,3]')
-    res.end()
+    res.writeHead(200,{'Content-Type':"text/plain"})
+    users().then(response=>{
+        res.write(JSON.stringify(response))
+        res.end()
+    })
+
 }).listen(5000,function(){
     console.log('Server is running on 5000 port')
 })
