e154549981f8966d09fb1daf693c6ae8e217aa48
injecti0n <9992685+injecti0n@users.noreply.github.com>
2021-01-26 11:23:33 +0100

    react.js intro tutorial

---
+++ REACTJS/tutorial1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './components/footer'
+import Header from './components/header'
+import Home from './components/home'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Home />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
